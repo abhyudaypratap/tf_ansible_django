b8282b289f0ee653cf727ac1302555bd721c310a
ABHYUDAY PRATAP SINGH <abhyudaypratap@outlook.com>
2019-07-28 23:58:33 +0530

    open only specific port

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -7,6 +7,27 @@ resource "aws_instance" "webapp" {
   ami           = "ami-02eac2c0129f6376b"
   instance_type = "t2.micro"
 
+  ingress {
+    from_port   = "22"
+    to_port     = "22"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = "80"
+    to_port     = "80"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   provisioner "local-exec" {
     command = "echo ${aws_instance.webapp.public_ip} > ip_address.txt"
   }
